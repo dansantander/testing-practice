d1618b934de20f9622ab9a5c0a12c0346935468f
Daniel Santander <danyvo1d@gmail.com>
2020-08-21 15:20:22 -0500

    Finished cypher module and testing

--- modules/cypher.js
+++ modules/cypher.js
@@ -1,28 +1,74 @@
 const Cypher = (() => {
-  const translator = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  const translatorUpper = translator.join().toUpperCase().split(',');
-  const result = []
+  const translatorLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  const translatorUpper = translatorLower.join().toUpperCase().split(',');
 
-  const encrypt = (string, number) => {
-    string.split('').forEach(element => {
-      if element.match(/[a-zA-Z]/) {
+  const encrypt = (string, shiftNumber = 0) => {
+    if (Number.isNaN(parseInt(shiftNumber, 10))) {
+      shiftNumber = 0;
+    }
 
+    if (typeof string !== 'string') {
+      throw new Error('Parameter is not a string');
+    }
+
+    const result = [];
+    string.split('').forEach((element) => {
+      if (element.match(/[a-zA-Z]/)) {
+        if (element === element.toUpperCase()) {
+          const translate = translatorUpper.findIndex(letter => letter === element);
+          let remainder = (translate + shiftNumber) % 25;
+          if (remainder < 0) {
+            remainder += 26;
+          }
+          result.push(translatorUpper[remainder]);
+        } else {
+          const translate = translatorLower.findIndex(letter => letter === element);
+          let remainder = (translate + shiftNumber) % 25;
+          if (remainder < 0) {
+            remainder += 26;
+          }
+          result.push(translatorLower[remainder]);
+        }
       } else {
-        result.push(element)
-      } 
+        result.push(element);
+      }
     });
-    return result.join(' ')
-  };
-  const decrypt = (string, number) => {
-
+    return result.join('');
   };
 
-  if (character == character.toUpperCase()) {
-    alert ('upper case true');
-   }
-   if (character == character.toLowerCase()){
-    alert ('lower case true');
-   }
+  const decrypt = (string, shiftNumber = 0) => {
+    if (Number.isNaN(parseInt(shiftNumber, 10))) {
+      shiftNumber = 0;
+    }
+
+    if (typeof string !== 'string') {
+      throw new Error('Parameter is not a string');
+    }
+
+    const result = [];
+    string.split('').forEach((element) => {
+      if (element.match(/[a-zA-Z]/)) {
+        if (element === element.toUpperCase()) {
+          const translate = translatorUpper.findIndex(letter => letter === element);
+          let remainder = (translate - shiftNumber) % 25;
+          if (remainder < 0) {
+            remainder += 26;
+          }
+          result.push(translatorUpper[remainder]);
+        } else {
+          const translate = translatorLower.findIndex(letter => letter === element);
+          let remainder = (translate - shiftNumber) % 25;
+          if (remainder < 0) {
+            remainder += 26;
+          }
+          result.push(translatorLower[remainder]);
+        }
+      } else {
+        result.push(element);
+      }
+    });
+    return result.join('');
+  };
 
   return {
     encrypt,
